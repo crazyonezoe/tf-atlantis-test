4c4b8722f601eb7b5d1293110db41c93c0131c0a
Zoe Ponsler <zponsler@amazon.com>
2021-03-10 13:21:44 -0800

    added files

---
+++ example.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region  = "us-west-2"
+  version = "~> 2.0"
+}
+#ec2 instance using the parameter store in the name of it
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = "subnet-cc0c60e7"
+  associate_public_ip_address = true
+  root_block_device {
+    volume_type = "standard"
+    volume_size = 30
+    encrypted   = true
+  }
+  tags = {
+    Name       = "HelloWorld"
+    Enviroment = "Dev"
+  }
+  security_groups = ["sg-bfe0bffd"]
+}
+
+
+# module "ec2_test" {
+#   source   = "./module/"
+#   ami      = data.aws_ami.ubuntu.id
+#   type     = "m5.large"
+#   subnet   = "subnet-cc0c60e7"
+#   ip       = true
+#   vol_type = "standard"
+#   vol_size = 30
+#   sg       = "sg-bfe0bffd"
+#   name     = "test"
+#   env      = "dev"
+# }
